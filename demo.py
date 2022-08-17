fbbb3b0ad5ea654e2a7f4d55a47412507bf58c42
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2022-08-17 01:15:17 -0700

    Add files via upload
    python program

---
+++ demo.py
@@ -0,0 +1,63 @@
+#Assignment-3: Type Conversion
+#1. Write a python script to convert a number into str type.
+
+from ssl import OP_NO_TLSv1_1
+
+
+x=1234
+print(str(x))
+
+#2. Write a python script to print Unicode of the character ‘m’
+
+a='m'
+print(ord(a))
+
+#3. Write a python script to print character representation of a given unicode 100.
+
+b=100
+print(chr(b))
+
+# 4. Write a python script to print any number and its binary equivalent
+
+b1=1234
+print(bin(b1))
+
+# 5. Write a python script to print any number and its octal equivalent.
+
+o_1=24233
+print(oct(o_1))
+
+# 6. Write a python script to print any number and its hexadecimal equivalent.
+
+h_1=2342424
+print(hex(h_1))
+
+# 7. Write a python script to store binary number 1100101 in a variable and print it in
+# decimal format.
+
+b2=0b1100101
+print(b2)
+
+#8. Write a python script to store a hexadecimal number 2F in a variable and print it in
+# octal format.
+
+h2=0x2f
+print(oct(h2))
+
+# 9. Write a python script to store an octal number 125 in a variable and print it in binary
+# format.
+
+o_2=0o125
+print(o_2)
+
+# 10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and
+# display the result in binary format.
+o_3=0o25
+h_3= 0x39
+print("sum fo oct and hex number is ",o_3+h_3)
+
+
+
+
+
+

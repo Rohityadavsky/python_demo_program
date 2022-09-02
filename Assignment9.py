7c6fa8e1dac34417b1bea449881bb5bf8d84f6bb
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2022-09-02 00:40:06 -0700

    Assignment9

---
+++ Assignment9.py
@@ -0,0 +1,51 @@
+# Assignment - 9 Full Stack Web Development using Python MySirG
+
+# While Loop
+
+# 1. Write a python script to print MySirG N times on the screen
+n=int(input("Enter a number:"))
+i=1
+while i<=n:
+    print("MySirG")
+    i+=1
+
+# 2. Write a python script to print first N natural numbers
+n=int(input("Enter a number"))
+i=1
+while i<=n:
+    print(i,end=" ")
+    i=i+1
+# 3. Write a python script to print first N natural numbers in reverse order
+
+n=int(input("Enter a number"))
+i=1
+while i<=n:
+    print(n,end=" ")
+    n=n-1
+# 4. Write a python script to print first N odd natural numbers
+n=int(input("Enter a number:"))
+i=1
+while i<=n:
+    print(i,end="  ")
+    i=i+2
+# 5. Write a python script to print first N odd natural numbers in reverse order
+n=int(input("Enter a number:"))
+i=1
+while i<=n:
+    print(n-1,end=" ")
+    n=n-2
+# 6. Write a python script to print first N even natural numbers
+n=int(input("Enter a number:"))
+i=1
+while i<=n:
+    print(i,end=" ")
+    i+=1
+# 7. Write a python script to print first N even natural numbers in reverse order
+n=int(input("Enter a number:"))
+i=1
+while i<=n:
+    print(n,end=" ")
+    n=n-2
+# 8. Write a python script to print squares of first N natural numbers
+# 9. Write a python script to print cubes of first N natural numbers
+# 10. Write a python script to print first 10 multiples of N

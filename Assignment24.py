2b4c48f84aa1abeb68ec10c10786661a108ef5f5
Rohit yadav <110348209+Rohityadavsky@users.noreply.github.com>
2022-10-08 02:01:46 -0700

    Update Assignment24.py

--- Assignment24.py
+++ Assignment24.py
@@ -3,28 +3,36 @@
 # Classes and Objects
 
 # 1. Write a python program to create a user class with 3 properties : name, age, email.
-
 class user:
-    name='rohit'
-    age=22
-    email='rohityadavcksy@gmail.com'
-
-u1=user()
-print(u1.name)
-print(u1.age)
-print(u1.email)
-
-
+    name="rohit"
+    age=23
+    email="rohit@gmail.com"
+obj=user()
+# print(obj.name)
+# print(obj.age)
+# print(obj.email)
+print(user.name)
+print(user.age)
+print(user.email)
 
 
 # 2. Write a python program to create a user class with a method to greet the user.
 
 class user:
-    def greet(self):
+
+    def greet_user(self):
         print("Congratulations")
 
 u1=user()
-print(u1.greet())
+print(u1.greet_user())
+
+#  or
+
+class user:
+    def __init__(self):
+        self.user="Greet user"
+obj=user()
+print(obj.user)
 
 
 
@@ -34,109 +42,145 @@ print(u1.greet())
 
 
 class user:
-    def __init__(self,p,r):
-        self.value1=p
-        self.value2=r
-    def show_data(self):
-        print(self.value1,self.value2)
 
-obj=user(30,50)
-obj2=user(100,200)
-obj.show_data()
-obj2.show_data()
+    def data(self):
+        print("All Information of user")
+
+obj=user()
+obj.name="MysirG"
+obj.place="Bhopal"
+
+obj2=user()
+obj2.email="mysig@gmail.com"
+#object first
+print(obj.name)
+print(obj.place)
+#object second
+print(obj2.email)
+    
+
 
 
 
 # 4. Write a python program to init default values for user object using __init__ method.
 
-
-class student:
+class user:
     def __init__(self):
-        self.marks=10
+        self.name="rohit"
+        self.age=23
+        self.email="rohit@gmail.com"
 
     def show(self):
-        print(self.marks)
+        print(self.name,self.age,self.email)
 
-std=student()
-std.show()
+obj=user()
+obj.show()
+
+#  or
+
+class user:
+    def __init__(self):
+        self.name="xyz"
+        self.email="xyz@gamilc.com"
+
+obj=user()
+print(obj.name,obj.email)
+
 
 
 
 # 5. Write a python program to delete the age property of the user.
 
-#doubt
+class user:
+    def __init__(self):
+        self.name="mysir"
+        self.age=50
 
 
+    def delete(self):
+        print(self.name)
+        del self.age
+        
+
+obj=user()
+obj.delete()
 
 
 
 # 6. Write a python program to create 3 user objects and find the youngest of all.
 
-#dobut
+class user:
+    def __init__(self,age):
+        self.age=age
+
+    def youngest(self):
+        if s1.age>s2.age and s1.age>s3.age:
+            print("s1 youngest")
+        elif s2.age>s1.age and s2.age>s3.age:
+            print("s2 youngest")
+        else:
+            print("s3 youngest")
+
+s1=user(40)
+s2=user(43)
+s3=user(39)
+print(s1.youngest())
+        
+
 
 
 
 # 7. Write a python program to create a Laptop class with 4 attributes (brand, ram, cpu,
 # hdd) and 2 methods (showConfig() to print the values, __init__() to initialize the
 # values).
-class laptop:
+# class laptop:
 
-    def __init__(self,r,c,h,b):
-        self.ram=r
-        self.cpu=c
-        self.hdd=h
-        self.brand=b
+class laptop:
+    def __init__(self):
+        self.brand="HP"
+        self.ram=32
+        self.cpu="i9"
 
     def showconfig(self):
-        print(self.ram,self.cpu,self.hdd,self.brand)
-        
+        return self.brand,self.ram,self.cpu
 
-com1=laptop(16,"i9","1TB","HP")
-com2=laptop(32,"i11","2TB","Macbook")
-print(com1.showconfig())
-print(com2.showconfig())
+
+lap=laptop()
+print(lap.showconfig())
+
 
 # 8. WRT 7th Question, create 3 Laptop objects and add them to the list in the sorted
 # order based on the ram size.
 
-class laptop:
-    def __init__(self,r,c,h,b):
-        self.ram=r
-        self.cpu=c
-        self.hdd=h
-        self.brand=b
 
-    def showconfig(self):
-        print(self.ram,self.cpu,self.hdd,self.brand)
-        
-
-com1=laptop(16,"i9","1TB","HP")
-com2=laptop(32,"i11","2TB","Macbook")
-com3=laptop(64,"i12","5TB","Macbook")
-
-print(com1.showconfig())
-print(com2.showconfig())
-print(com3.showconfig())
-li=[16,32,64]
-print(sorted(li))
-
+#confusion  
 
 
 
 
 # 9. Write a python program to create a School class and 3 instance variables and 1 class
 # variable.
+
 class school:
-    school="ineuron"
+    school_name="xyz"
 
     def __init__(self):
-        self.name="Rohit"
-        self.age=22
-        self.email="rohit@gamil.com"
+        self.principal_name="Gajodhar"
+        self.teacher_number=50
+        self.all_number_of_student=1000
+
+    def school_data(self):
+        print(self.principal_name)
+        print(self.teacher_number)
+        print(self.all_number_of_student)
+
+    @classmethod
+    def show_school_name(cls):
+        return cls.school_name
 
 obj=school()
-print(school.school)
-
+obj.show_school_name()
+obj.school_data()
 
 
 
@@ -144,15 +188,14 @@ print(school.school)
 # __init__() method in the class to initialize instance object variables. Also define
 # instance methods to input fields and display field values
 
+class employee:
+    def __init__(self,empid,name,salary):
+        self.empid=empid
+        self.name=name
+        self.salary=salary
 
-class Employee:
-    def __init__(self):
-        self.empid=100
-        self.name="Maxwell"
-        self.salary="1.1b"
+    def show_employ(self):
+        return self.empid,self.name,self.salary
 
-    def show_all_data(self):
-        print(self.empid,self.name,self.salary)
-
-obj=Employee()
-obj.show_all_data()
+obj=employee(1,"pramod",25000)
+print(obj.show_employ())
